a0ab5f35d8933ed1f1db0db73ff142010d9b3dfd
Arthur <arthurd800@wp.pl>
2020-08-31 15:57:28 +0200

    app takes users location and writes it in firebase under geolocation prop

--- src/components/home/GMap.vue
+++ src/components/home/GMap.vue
@@ -6,6 +6,7 @@
 
 <script>
 import firebase from 'firebase';
+import db from '@/firebase/init';
 export default {
     name: 'GMap',
     data() {
@@ -29,8 +30,48 @@ export default {
         },
     },
     mounted() {
-        this.renderMap();
-        console.log(firebase.auth().currentUser);
+        //get current user
+        let user = firebase.auth().currentUser;
+        console.log(user);
+        //get user geolocation
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(
+                (pos) => {
+                    this.lat = pos.coords.latitude;
+                    this.lng = pos.coords.longitude;
+                    // find the user record and then update records
+                    db.collection('users')
+                        .where('user_id', '==', user.uid)
+                        .get()
+                        .then((snapshot) => {
+                            snapshot.forEach((doc) => {
+                                db.collection('users')
+                                    .doc(doc.id)
+                                    .update({
+                                        geolocation: {
+                                            lat: pos.coords.latitude,
+                                            lng: pos.coords.longitude,
+                                        },
+                                    });
+                            });
+                        })
+                        .then(() => {
+                            this.renderMap();
+                        });
+                },
+                (err) => {
+                    console.log(err);
+                    this.renderMap();
+                },
+                {
+                    maximumAge: 60000,
+                    timeout: 3000,
+                }
+            );
+        } else {
+            // position center by different values
+            this.renderMap();
+        }
     },
 };
 </script>
